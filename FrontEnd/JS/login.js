e632a43c1166f6e52f4d6c740c11ec9c619719bf
beckysanchez <rebeca.carrillosn@uanl.edu.mx>
2026-02-23 20:30:49 -0600

    validaciones de correo y contrasena
    validaciones agregadas

--- FrontEnd/JS/login.js
+++ FrontEnd/JS/login.js
@@ -1,5 +1,23 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) 
 { event.preventDefault();
-    console.log('form de inicio sesion enviado');});
 
-    
+    const usuario = document.getElementById('usuario').value;
+    const password = document.getElementById('password').value;
+
+    const validEmail= /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!validEmail.test(usuario)) {
+        alert('Correo invalido, debes agregar un caracter de puntuacion.');
+        return;
+    }
+
+    const validPassword = /^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/;
+    if (!validPassword.test(password)) {
+        alert('Contraseña invalida, debe contener al menos 8 caracteres, una mayuscula, una minuscula, un numero y un caracter especial.');
+        return;
+    }
+
+    console.log(usuario, password);
+});
+
+
+
